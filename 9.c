23f9bb68d7755da2693b16f34c42bc4923abfd73
meanishaw <127970045+meanishaw@users.noreply.github.com>
2025-09-22 15:43:54 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insertNode() {
+    int val, choice;
+    printf("\nEnter node value (-1 for no node): ");
+    scanf("%d", &val);
+    if (val == -1) return NULL;
+
+    struct Node* root = createNode(val);
+
+    printf("Enter left child of %d:\n", val);
+    root->left = insertNode();
+    printf("Enter right child of %d:\n", val);
+    root->right = insertNode();
+
+    return root;
+}
+
+void inorder(struct Node* root) {
+    if (root) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void preorder(struct Node* root) {
+    if (root) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct Node* root) {
+    if (root) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+    printf("Create binary tree (-1 for no node):\n");
+    root = insertNode();
+
+    printf("\nInorder Traversal: ");
+    inorder(root);
+    printf("\nPreorder Traversal: ");
+    preorder(root);
+    printf("\nPostorder Traversal: ");
+    postorder(root);
+
+    return 0;
+}
